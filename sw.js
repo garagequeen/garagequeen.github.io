0589af8c196f35e69b7a8abdb66fcd9078bdc5cd
garagequeen <garagequeen.app@gmail.com>
2026-03-08 09:54:36 +0100

    dynamic cache Update sw.js

--- sw.js
+++ sw.js
@@ -1,23 +1,27 @@
-const CACHE = "garage-cache-v3"
+const CACHE = “garage-cache-v4”
 
-const ASSETS = [
-"./",
-"./index.html",
-"./manifest.json"
-]
-
-self.addEventListener("install", e => {
-
-e.waitUntil(
-caches.open(CACHE).then(cache => cache.addAll(ASSETS))
-)
-
+self.addEventListener(“install”, e => {
+self.skipWaiting()
 })
 
-self.addEventListener("fetch", e => {
-
-e.respondWith(
-caches.match(e.request).then(res => res || fetch(e.request))
+self.addEventListener(“activate”, e => {
+e.waitUntil(
+caches.keys().then(keys =>
+Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
 )
+)
+self.clients.claim()
+})
 
+// network-first: всегда берём свежий файл, кеш только как fallback
+self.addEventListener(“fetch”, e => {
+e.respondWith(
+fetch(e.request)
+.then(res => {
+const clone = res.clone()
+caches.open(CACHE).then(cache => cache.put(e.request, clone))
+return res
+})
+.catch(() => caches.match(e.request))
+)
 })
