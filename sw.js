7de168229eae87f419c21f3cd8a2585601d96507
garagequeen <garagequeen.app@gmail.com>
2026-03-09 20:26:49 +0100

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,31 @@
+const CACHE = “garage-cache-v5”
+const PRECACHE = [”/”, “/index.html”, “/app.css”, “/app.js”]
+
+self.addEventListener(“install”, e => {
+self.skipWaiting()
+e.waitUntil(
+caches.open(CACHE).then(cache => cache.addAll(PRECACHE))
+)
+})
+
+self.addEventListener(“activate”, e => {
+e.waitUntil(
+caches.keys().then(keys =>
+Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+)
+)
+self.clients.claim()
+})
+
+// network-first: всегда берём свежий файл, кеш только как fallback
+self.addEventListener(“fetch”, e => {
+e.respondWith(
+fetch(e.request)
+.then(res => {
+const clone = res.clone()
+caches.open(CACHE).then(cache => cache.put(e.request, clone))
+return res
+})
+.catch(() => caches.match(e.request))
+)
+})
