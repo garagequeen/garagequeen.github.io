67ba2a0efeba92458c46d5fdb1cf13507e6664df
garagequeen <garagequeen.app@gmail.com>
2026-03-20 08:37:42 +0200

    v0.6.8 Update app.js

--- js/app.js
+++ js/app.js
@@ -1,5 +1,5 @@
 
-const APP_VERSION = 'v0.6.7'
+const APP_VERSION = 'v0.6.8'
 const SUPA_URL = "https://fqmmlntmpybijmvrsfxx.supabase.co"
 const SUPA_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZxbW1sbnRtcHliaWptdnJzZnh4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzI4MTYyMzMsImV4cCI6MjA4ODM5MjIzM30.HeiCco9pyNwDKUJJhA5Af6Yh7AIRZH5GGlvr4BFOcXk"
 const sb = supabase.createClient(SUPA_URL, SUPA_KEY, { auth: { flowType: 'pkce' } })
@@ -15,12 +15,6 @@ window.addEventListener('load', () => {
     document.getElementById('versionLogin').innerText = APP_VERSION
   } catch(e) {}
 
-  // const vf = document.getElementById('versionFooter')
-  // const vl = document.getElementById('versionLogin')
-  // if (vf) vf.inner.Text = APP_VERSION
-  // if (vl) vl.inner.Text = APP_VERSION
-  //document.getElementById('versionFooter').innerText = APP_VERSION - right after checkUser
-  //document.getElementById('versionLogin').innerText = APP_VERSION - right after checkUser
 })
 function login() {
   sb.auth.signInWithOAuth({ provider:"google", options:{ redirectTo: window.location.origin } })
